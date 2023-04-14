dec1e593050478feca68aa22837b5c945c072b05
yankikucuk <kendinikertenkelebek@me.com>
2023-04-14 18:06:05 +0300

    feat: added embed colors

---
+++ src/constants/discord/colors.js
@@ -0,0 +1,13 @@
+export default {
+  primary: '',
+  success: '',
+  information: [252, 215, 3],
+  warning: [252, 53, 3],
+  socialMedia: {
+    twitch: '',
+    youtube: '',
+    twitter: '',
+    instagram: '',
+    facebook: '',
+  },
+};
